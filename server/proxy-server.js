845ea2e7c06f3de84becfb72d0ef4b6875cbea1a
Yash Kamble <61692717+YashKamble365@users.noreply.github.com>
2025-09-30 18:11:58 +0530

    Update proxy-server.js

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -304,7 +304,7 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 3001;
-const HOST = process.env.HOST || '0.0.0.0';
+const HOST = process.env.HOST || 3000;
 
 app.listen(PORT, HOST, () => {
   console.log('🚀 WRIS Proxy Server Started!');
